39f38b26e7869c3332e008044dc44502929c0914
Naissus <Dusan.Antic6@gmail.com>
2023-03-23 14:25:41 +0100

    Tests on Amazon

--- cypress/e2e/cypresstests.cy.js
+++ cypress/e2e/cypresstests.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://google.com')
-  })
-})
+describe('Amazon website tests', () => {
+  beforeEach(() => {
+    cy.visit('https://www.amazon.com/');
+  });
+
+  it('Loads Amazon homepage', () => {
+    cy.url().should('contain', 'amazon.com');
+  });
+
+  it('Displays Amazon logo', () => {
+    cy.get('#nav-logo-sprites').should('exist');
+  });
+
+  it('Displays Amazon search bar', () => {
+    cy.get('#twotabsearchtextbox').should('exist');
+  });
+
+  it('Displays Amazon footer', () => {
+    cy.get('#navFooter').should('exist');
+  });
+
+  it('Displays Amazon language dropdown', () => {
+    cy.get('#icp-nav-flyout').should('exist');
+  });
+});
