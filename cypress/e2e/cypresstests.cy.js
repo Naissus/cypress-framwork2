4121fc0cbfd13a07dce7f8217707210a6a3aab45
Naissus <Dusan.Antic6@gmail.com>
2023-03-24 15:34:56 +0100

    Uppgift KK2

--- cypress/e2e/cypresstests.cy.js
+++ cypress/e2e/cypresstests.cy.js
@@ -1,12 +1,12 @@
 describe('Search for a computer screen', () => {
-  it('Visits Amazon.com and searches for a computer screen', () => {
+  it('Visits Amazon and searches for a computer screen', () => {
     cy.visit('https://www.amazon.com/')
     cy.get('#twotabsearchtextbox').type('computer screen{enter}')
     cy.get('.s-result-item').should('have.length.gt', 0)
   })
 })
 describe('Search for computer chairs within a specific price range', () => {
-  it('Visits Amazon.com and searches for books within a specific price range', () => {
+  it('Visits Amazon and searches for computer chair within a specific price range', () => {
     cy.visit('https://www.amazon.com/')
     cy.get('#twotabsearchtextbox').type('computer chair')
     cy.get('#twotabsearchtextbox').type('{enter}')
@@ -17,7 +17,7 @@ describe('Search for computer chairs within a specific price range', () => {
   })
 })
 describe('Log in with email and password', () => {
-  it('Visits Amazon.com and logs in with email and password', () => {
+  it('Visits Amazon and logs in with email and password', () => {
     cy.visit('https://www.amazon.com/')
     cy.get('#nav-link-accountList').click()
     cy.get('#ap_email').type('example@email.com')
@@ -27,18 +27,18 @@ describe('Log in with email and password', () => {
     cy.url().should('include', '/ap/signin')
   })
 })
-describe('Change language to German', () => {
-  it('Visits Amazon.com and changes the language to German', () => {
-    cy.visit('https://www.amazon.com/')
-    cy.get('#icp-nav-flyout', { timeout: 10000}).click()
-    cy.contains('Deutsch - DE', { timeout: 10000 }).click()
-    cy.get('.a-button-input[type=submit]').click()
-    cy.url().should('include', 'https://www.amazon.com/')
-  })
-})
+describe('Amazon Music and Podcasts', () => {
+  it('should navigate to Podcasts via the hamburger menu', () => {
+    cy.visit('https://www.amazon.com/');
+    cy.get('#nav-hamburger-menu').click();
+    cy.get('.hmenu-item').contains('Amazon Music').click();
+    cy.get('.hmenu-item').contains('Podcasts').click();
+    cy.url().should('contain', 'https://www.amazon.com/music');
+  });
+});
 describe('View the cart', () => {
-  it('Visits Amazon.com and views the cart', () => {
-    cy.visit('https://www.amazon.com')
+  it('Visits Amazon and views the cart', () => {
+    cy.visit('https://www.amazon.com', {timeout: 5000})
     cy.get('#nav-cart').click()
     cy.get('#nav-cart-count-container', { timeout: 10000 }).should('be.visible')
     cy.url().should('include', '/gp/cart/view.html')
