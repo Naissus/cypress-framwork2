68e66c3c41c1bebb1f193ef56c75e67cdad88730
Naissus <Dusan.Antic6@gmail.com>
2023-03-24 15:01:14 +0100

    Final Push

--- cypress/e2e/cypresstests.cy.js
+++ cypress/e2e/cypresstests.cy.js
@@ -1,25 +1,45 @@
-describe('Amazon website tests', () => {
-  beforeEach(() => {
-    cy.visit('https://www.amazon.com/');
-  });
-
-  it('Loads Amazon homepage', () => {
-    cy.url().should('contain', 'amazon.com');
-  });
-
-  it('Displays Amazon logo', () => {
-    cy.get('#nav-logo-sprites').should('exist');
-  });
-
-  it('Displays Amazon search bar please', () => {
-    cy.get('#twotabsearchtextbox').should('exist');
-  });
-
-  it('Displays Amazon footer', () => {
-    cy.get('#navFooter').should('exist');
-  });
-
-  it('Displays Amazon language dropdown', () => {
-    cy.get('#icp-nav-flyout').should('exist');
-  });
-});
+describe('Search for a computer screen', () => {
+  it('Visits Amazon.com and searches for a computer screen', () => {
+    cy.visit('https://www.amazon.com/')
+    cy.get('#twotabsearchtextbox').type('computer screen{enter}')
+    cy.get('.s-result-item').should('have.length.gt', 0)
+  })
+})
+describe('Search for computer chairs within a specific price range', () => {
+  it('Visits Amazon.com and searches for books within a specific price range', () => {
+    cy.visit('https://www.amazon.com/')
+    cy.get('#twotabsearchtextbox').type('computer chair')
+    cy.get('#twotabsearchtextbox').type('{enter}')
+    cy.get('#low-price').type('100')
+    cy.get('#high-price').type('200')
+    cy.get('.a-button-input[type=submit]').click()
+    cy.get('.s-result-item').should('have.length.gt', 0)
+  })
+})
+describe('Log in with email and password', () => {
+  it('Visits Amazon.com and logs in with email and password', () => {
+    cy.visit('https://www.amazon.com/')
+    cy.get('#nav-link-accountList').click()
+    cy.get('#ap_email').type('example@email.com')
+    cy.get('#continue').click()
+    cy.get('#ap_password').type('password123')
+    cy.get('#signInSubmit').click()
+    cy.url().should('include', '/ap/signin')
+  })
+})
+describe('Change language to German', () => {
+  it('Visits Amazon.com and changes the language to German', () => {
+    cy.visit('https://www.amazon.com/')
+    cy.get('#icp-nav-flyout').click()
+    cy.contains('Deutsch - DE').click()
+    cy.get('.a-button-input[type=submit]').click()
+    cy.url().should('include', 'https://www.amazon.com/')
+  })
+})
+describe('View the cart', () => {
+  it('Visits Amazon.com and views the cart', () => {
+    cy.visit('https://www.amazon.com/')
+    cy.get('#nav-cart-count-container').click()
+    cy.url().should('include', '/gp/cart')
+  })
+})
